4a625526e0ab6b34e43fc88a633151e4f585b26e
Vikram Singh <vikramshivraj2005@gmail.com>
2026-07-28 16:18:25 +0530

    migrate MySQL connection to Aiven with SSL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,18 @@
-const mysql=require("mysql2");
-const connection =mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-     password: process.env.DB_PASSWORD,
+const mysql = require("mysql2");
+const fs = require("fs");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+
+  ssl: {
+    ca: fs.readFileSync("/etc/secrets/ca.pem"),
+  },
 });
+
 connection.connect((err) => {
   if (err) {
     console.log("Database Error", err);
